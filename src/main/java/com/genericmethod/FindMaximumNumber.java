f696dc671bc4ddc528cb0713e9ee4ad7aa441c87
Jeswin2711 <jeswin2711@gmail.com>
2022-07-19 17:28:31 +0530

    Added:UseCase2

--- src/main/java/com/genericmethod/FindMaximumNumber.java
+++ src/main/java/com/genericmethod/FindMaximumNumber.java
@@ -9,19 +9,41 @@ public class FindMaximumNumber
     {
         if (a.compareTo(b) > 0 && a.compareTo(c) > 0)
         {
-            System.out.println("A is Maximum Value");
+            System.out.println("A is Maximum Value\n");
         }
         else if (b.compareTo(c) > 0 && b.compareTo(a) > 0)
         {
-            System.out.println("B is Maximum Value");
+            System.out.println("B is Maximum Value\n");
         }
         else if (c.compareTo(a) > 0 && c.compareTo(b) > 0)
         {
-            System.out.println("C is Maximum Value");
+            System.out.println("C is Maximum Value\n");
         }
         else
         {
-            System.out.println("All Values are Equal");
+            System.out.println("All Values are Equal\n");
+        }
+    }
+    /*
+    Function to Find the Maximum Float Value b/w  d, e  and f
+     */
+    void findMaxFloat(Float a , Float b ,Float c)
+    {
+        if (a.compareTo(b) > 0 && a.compareTo(c) > 0)
+        {
+            System.out.println("D is Maximum Value\n");
+        }
+        else if (b.compareTo(c) > 0 && b.compareTo(a) > 0)
+        {
+            System.out.println("E is Maximum Value\n");
+        }
+        else if (c.compareTo(a) > 0 && c.compareTo(b) > 0)
+        {
+            System.out.println("F is Maximum Value\n");
+        }
+        else
+        {
+            System.out.println("All Values are Equal\n");
         }
     }
 }
--- src/main/java/com/genericmethod/MaximumNumber.java
+++ src/main/java/com/genericmethod/MaximumNumber.java
@@ -11,6 +11,7 @@ public class MaximumNumber extends FindMaximumNumber
     {
         System.out.println("Maximum Number Problem\n");
         Scanner scanner = new Scanner(System.in);
+        //Integer Part
         System.out.println("Enter Number A");
         Integer a = scanner.nextInt();
         System.out.println("Enter Number B");
@@ -20,5 +21,15 @@ public class MaximumNumber extends FindMaximumNumber
         FindMaximumNumber find = new FindMaximumNumber();
         //calling FindMaximumNumber function using object to calculate the Maximum Integer
         find.findMaxInteger(a , b, c);
+
+        //Float Part
+        System.out.println("Enter Number D");
+        Float d = scanner.nextFloat();
+        System.out.println("Enter Number E");
+        Float e = scanner.nextFloat();
+        System.out.println("Enter Number F");
+        Float f = scanner.nextFloat();
+        //calling FindMaximumNumber function using object to calculate the Maximum Integer
+        find.findMaxFloat(d , e , f);
     }
 }
