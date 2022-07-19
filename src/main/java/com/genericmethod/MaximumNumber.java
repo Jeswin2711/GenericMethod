e0d7189ab0082b3392de678296abae21951adba6
Jeswin2711 <jeswin2711@gmail.com>
2022-07-19 17:19:21 +0530

    Added:UseCase1

--- src/main/java/com/genericmethod/MaximumNumber.java
+++ src/main/java/com/genericmethod/MaximumNumber.java
@@ -1,9 +1,20 @@
 package com.genericmethod;
 
-public class MaximumNumber
+import java.util.Scanner;
+
+public class MaximumNumber extends FindMaximumNumber
 {
     public static void main(String[] args)
     {
-        System.out.println("Maximum Number Problem");
+        System.out.println("Maximum Number Problem\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Number A");
+        Integer a = scanner.nextInt();
+        System.out.println("Enter Number B");
+        Integer b = scanner.nextInt();
+        System.out.println("Enter Number C");
+        Integer c = scanner.nextInt();
+        FindMaximumNumber find = new FindMaximumNumber();
+        find.findMaxInteger(a , b, c);
     }
 }
