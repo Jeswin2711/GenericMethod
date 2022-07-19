7807c919f77106eac2f5e5c7475c78909db06c1e
Jeswin2711 <jeswin2711@gmail.com>
2022-07-19 18:10:40 +0530

    Added:UseCase4

--- src/main/java/com/genericmethod/MaximumNumber.java
+++ src/main/java/com/genericmethod/MaximumNumber.java
@@ -2,44 +2,64 @@ package com.genericmethod;
 
 import java.util.Scanner;
 
-public class MaximumNumber extends FindMaximumNumber
+public class MaximumNumber
 {
     /*
     Main Function to Get Values from User and to pass to the respective Function using Objects...
      */
     public static void main(String[] args)
     {
-        System.out.println("Maximum Number Problem\n");
         Scanner scanner = new Scanner(System.in);
-        //Integer Part
-        System.out.println("Enter Number A");
-        Integer a = scanner.nextInt();
-        System.out.println("Enter Number B");
-        Integer b = scanner.nextInt();
-        System.out.println("Enter Number C");
-        Integer c = scanner.nextInt();
-        FindMaximumNumber find = new FindMaximumNumber();
-        //calling findMaxInteger function in class FindMaximumNumber using object to calculate the Maximum Integer
-        find.findMaxInteger(a , b, c);
-
-        //Float Part
-        System.out.println("Enter Number D");
-        Float d = scanner.nextFloat();
-        System.out.println("Enter Number E");
-        Float e = scanner.nextFloat();
-        System.out.println("Enter Number F");
-        Float f = scanner.nextFloat();
-        //calling findMaxFloat function in class FindMaximumNumber using object to calculate the Maximum Float
-        find.findMaxFloat(d , e , f);
-
-        //String Part
-        System.out.println("Enter String G");
-        String g = scanner.next();
-        System.out.println("Enter String H");
-        String h = scanner.next();
-        System.out.println("Enter String I");
-        String i = scanner.next();
-        //calling findMaxString function in class FindMaximumNumber using object to calculate the Maximum String
-        find.findMaxString(g , h , i);
+        System.out.println("Finding Maximum Number Using Generics");
+        System.out.println("-------------------------------------\n");
+        while (true) {
+            System.out.println("Enter 1 for INTEGER");
+            System.out.println("Enter 2 for FLOAT");
+            System.out.println("Enter 3 for STRING");
+            System.out.println("Enter 4 for EXIT");
+            int input = scanner.nextInt();
+            switch (input) {
+                case 1:
+                    System.out.println("Enter three Value of Integer to Find Max Value");
+                    //calling CalculateMax generic funtion to calculate the maximum value
+                    System.out.println("The Maximum Integer Value " + calculateMax(scanner.nextInt(), scanner.nextInt(), scanner.nextInt())+"\n");
+                    break;
+                case 2:
+                    System.out.println("Enter three Value of Float to Find Max Value");
+                    //calling CalculateMax generic funtion to calculate the maximum value
+                    System.out.println("The Maximum Float Value " + calculateMax(scanner.nextFloat(), scanner.nextFloat(), scanner.nextFloat())+"\n");
+                    break;
+                case 3:
+                    System.out.println("Enter three Value of String to Find Max Value");
+                    //calling CalculateMax generic funtion to calculate the maximum value
+                    System.out.println("The Maximum String Value " + calculateMax(scanner.next(), scanner.next(), scanner.next())+"\n");
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+            }
+        }
+    }
+    /*
+    Generic Method to Calculate The Maximum Value of Any Type
+     */
+    static <E extends Comparable> E calculateMax(E a , E b , E c)
+    {
+        if (a.compareTo(b) > 0 && a.compareTo(c) > 0)
+        {
+            return a;
+        }
+        else if (b.compareTo(c) > 0 && b.compareTo(a) > 0)
+        {
+            return b;
+        }
+        else if (c.compareTo(a) > 0 && c.compareTo(b) > 0)
+        {
+            return c;
+        }
+        else
+        {
+            return null;
+        }
     }
 }
